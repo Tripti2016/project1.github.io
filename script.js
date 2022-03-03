e262599190fb8de1cf43e88e0db30f2101debd6e
Tripti2016 <triptikhandelwal1@gmail.com>
2022-03-03 15:18:27 +0530

    MY project 1 travel website
    Its my project website on travel.

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
